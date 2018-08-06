89ea086b5027194e8cac27e530b4d1073ae420f2
ManHyuk <manhyuk@kw.ac.kr>
2018-08-06 16:52:57 +0900

    뿌듯

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import mh.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        mh.bj2309.exec();
+    }
+}
---
+++ src/mh/bj2309.java
@@ -0,0 +1,19 @@
+package mh;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class bj2309 {
+
+    public static void exec() throws Exception {
+        ArrayList<Integer> input = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 9; i++) {
+            input.add(sc.nextInt());
+        }
+
+
+
+    }
+}
