13751e9494e813c400dbc999044797ec9c7b4f50
manhyuk <manhyuk@kw.ac.kr>
2019-03-01 21:46:29 +0900

    add boj14888

--- src/Main.java
+++ src/Main.java
@@ -97,6 +97,7 @@ public class Main {
 //        problems.BOJ.bj1966.solve();
 //        problems.BOJ.bj1260.solve();
 //        problems.BOJ.bj5063.solve();
-        problems.BOJ.bj2231.solve();
+//        problems.BOJ.bj2231.solve();
+        problems.BOJ.bj14888.solve();
     }
 }
