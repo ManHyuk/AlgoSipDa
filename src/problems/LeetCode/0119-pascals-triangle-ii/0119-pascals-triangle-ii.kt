13a82463c1b4941a1d6bf9d4abd4bee6d5f4e662
ManHyuk <rnanhyuk@gmail.com>
2024-03-25 23:08:12 +0900

    Time: 155 ms (47.71%), Space: 37.1 MB (31.37%) - LeetHub

---
+++ src/problems/LeetCode/0119-pascals-triangle-ii/0119-pascals-triangle-ii.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun getRow(rowIndex: Int): List<Int> {
+        return generateSequence(listOf(1)) { prev -> listOf(1) + prev.windowed(2, 1).map { it.sum() } + listOf(1) }
+            .take(rowIndex + 1)
+            .toList()[rowIndex]     
+    }
+}
