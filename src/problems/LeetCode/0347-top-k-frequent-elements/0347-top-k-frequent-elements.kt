fc98e0404735377ab8860f195bb9fce0e0ae4f47
ManHyuk <rnanhyuk@gmail.com>
2024-07-07 14:03:57 +0900

    Time: 301 ms (25.13%), Space: 43.9 MB (30.98%) - LeetHub

---
+++ src/problems/LeetCode/0347-top-k-frequent-elements/0347-top-k-frequent-elements.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray =
+        nums.asSequence()
+            .groupingBy { it }
+            .eachCount()
+            .asSequence()
+            .sortedByDescending { it.value }
+            .map { it.key }
+            .take(k)
+            .toList()
+            .toIntArray()
+}
